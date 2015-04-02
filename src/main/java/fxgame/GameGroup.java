f9c31cb5480974459eccd4a0566e3acca94ace3e
Michael E. Cotterell <mepcotterell@gmail.com>
2015-04-02 14:58:12 -0400

    added the beginnings of the GameGroup class

---
+++ src/main/java/fxgame/GameGroup.java
@@ -0,0 +1,35 @@
+package fxgame;
+
+import javafx.scene.Group;
+import javafx.animation.Timeline;
+
+/**
+ * A <code>GameGroup</code> node is a {@link javafx.scene.Group} that updates
+ * its children via an associated {@link javafx.animation.Timeline}.
+ */
+public class GameGroup extends Group {
+
+    /** The target framerate for a <code>GameGroup</code>. */
+    public static final double TARGET_FPS = 60.0;
+
+    private final Timeline timeline = new Timeline(TARGET_FPS);
+
+    /** 
+     * Constructs a <code>GameGroup</code> object with default settings.
+     */
+    public GameGroup() {
+
+    } // GameGroup
+
+    /**
+     * Returns the {@link javafx.animation.Timeline} associated with this
+     * <code>GameGroup</code>.
+     *
+     * @return the associated timeline
+     */
+    public Timeline getTimeline() {
+	return this.timeline;
+    } // getTimeline
+
+} // GameGroup
+
